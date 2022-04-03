b806e4fdd4a6ebf563cfc24aac917eac2a47750a
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-03 00:39:50 -0700

    Create Post model

--- insta/models.py
+++ insta/models.py
@@ -6,7 +6,7 @@ class Profile(models.Model):
     """Model definition for Profile."""
 
     # TODO: Define fields here
-    profile_photo = models.ImageField(upload_to = 'profiles')
+    profile_photo = models.ImageField(upload_to = 'profiles/')
     bio = models.CharField(max_length=200)
 
     class Meta:
@@ -18,3 +18,22 @@ class Profile(models.Model):
     def __str__(self):
         """Unicode representation of Profile."""
         self.profile_photo
+
+class Image(models.Model):
+    """Model definition for Image."""
+
+    # TODO: Define fields here
+    image = models.ImageField(upload_to = 'posts/')
+    name = models.CharField(max_length=20)
+    caption = models.TextField()
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    class Meta:
+        """Meta definition for Image."""
+
+        verbose_name = 'Image'
+        verbose_name_plural = 'Images'
+
+    def __str__(self):
+        """Unicode representation of Image."""
+        pass
