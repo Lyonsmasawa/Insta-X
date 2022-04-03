032e20f1beb6ab10703e5b0584a30d5f149bfe04
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-03 04:10:55 -0700

    create add post form

--- insta/forms.py
+++ insta/forms.py
@@ -2,4 +2,11 @@ from django.forms import ModelForm
 from .models import Image
 
 class ImageForm(ModelForm):
-    
+    """Form definition for Image."""
+
+    class Meta:
+        """Meta definition for Imageform."""
+
+        model = Image
+        fields = '__all__'
+        exclude = ['owner']
