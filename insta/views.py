d81b5a4e6818a3235835b738827c263a28cfaeda
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-02 21:06:29 -0700

    views and templates

--- insta/views.py
+++ insta/views.py
@@ -1,6 +1,18 @@
+from multiprocessing import context
+from unicodedata import name
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    posts = [
+        {'id': 1, 'name': 'Design with me'},
+        {'id': 1, 'name': 'Design with me'},
+        {'id': 1, 'name': 'Design with me'},
+    ]
+    context = {'posts': posts}
+    return render(request, 'insta/home.html', context)
 
-    return render(request, 'main.html', {})
+def post(request):
+
+    context = {}
+    return render(request, 'insta/posts.html', context)
