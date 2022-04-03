346ace02247f227a8c04f40621677787b367bc40
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-02 21:29:29 -0700

    basic functionality

--- insta/urls.py
+++ insta/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('post/', views.post, name="post"),
+    path('post/<str:pk>', views.post, name="post"),
 ]
--- insta/views.py
+++ insta/views.py
@@ -3,16 +3,22 @@ from unicodedata import name
 from django.shortcuts import render
 
 # Create your views here.
+
+posts = [
+    {'id': 1, 'name': 'Design with me'},
+    {'id': 1, 'name': 'Design with me'},
+    {'id': 1, 'name': 'Design with me'},
+]
+
 def home(request):
-    posts = [
-        {'id': 1, 'name': 'Design with me'},
-        {'id': 1, 'name': 'Design with me'},
-        {'id': 1, 'name': 'Design with me'},
-    ]
     context = {'posts': posts}
     return render(request, 'insta/home.html', context)
 
-def post(request):
+def post(request, pk):
+    post = None
+    for i in posts:
+        if i['id'] == int(pk):
+            post = i
 
-    context = {}
+    context = {'post':post}
     return render(request, 'insta/posts.html', context)
