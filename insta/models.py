72913bf549d2885dddab5850f3aeb05c2ccca830
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-03 00:48:07 -0700

    Create Comments Model

--- insta/models.py
+++ insta/models.py
@@ -1,5 +1,5 @@
-from distutils.command.upload import upload
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
@@ -8,7 +8,9 @@ class Profile(models.Model):
     # TODO: Define fields here
     profile_photo = models.ImageField(upload_to = 'profiles/')
     bio = models.CharField(max_length=200)
-
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True) #auto_now takes a snapshot everytime a save occures while auto_now_add takes a snapshot only one the first time a save occures
+   
     class Meta:
         """Meta definition for Profile."""
 
@@ -26,6 +28,8 @@ class Image(models.Model):
     image = models.ImageField(upload_to = 'posts/')
     name = models.CharField(max_length=20)
     caption = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True) #auto_now takes a snapshot everytime a save occures while auto_now_add takes a snapshot only one the first time a save occures
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
     class Meta:
@@ -36,4 +40,25 @@ class Image(models.Model):
 
     def __str__(self):
         """Unicode representation of Image."""
-        pass
+        self.name
+
+class Comments(models.Model):
+    """Model definition for Comment."""
+
+    # TODO: Define fields here
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True) #auto_now takes a snapshot everytime a save occures while auto_now_add takes a snapshot only one the first time a save occures
+
+
+    class Meta:
+        """Meta definition for Comment."""
+
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+
+    def __str__(self):
+        """Unicode representation of Comment."""
+        self.body
