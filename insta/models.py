e96d11090ed09fae41e83d16a861928c81bbd309
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-03 00:33:21 -0700

    Create profile model

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,20 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    """Model definition for Profile."""
+
+    # TODO: Define fields here
+    profile_photo = models.ImageField(upload_to = 'profiles')
+    bio = models.CharField(max_length=200)
+
+    class Meta:
+        """Meta definition for Profile."""
+
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+
+    def __str__(self):
+        """Unicode representation of Profile."""
+        self.profile_photo
