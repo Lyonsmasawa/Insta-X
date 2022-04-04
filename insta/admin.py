15c7ddc5e2a7d6f6aea9a139905920c580ce88a9
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-04 09:18:10 -0700

    register follow model

--- insta/admin.py
+++ insta/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Image, Comment, Profile, Tag
+from .models import Image, Comment, Profile, Tag, Follow
 
 # Register your models here.
 admin.site.register(Tag)
 admin.site.register(Image)
 admin.site.register(Comment)
 admin.site.register(Profile)
+admin.site.register(Follow)
