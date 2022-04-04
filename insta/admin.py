a0156926ff61d76352952c58bedbf7cbfb1740d0
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-04 11:46:45 -0700

    register like model

--- insta/admin.py
+++ insta/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Image, Comment, Profile, Tag, Follow
+from .models import Image, Comment, Profile, Tag, Follow,Like
 
 # Register your models here.
 admin.site.register(Tag)
@@ -7,3 +7,4 @@ admin.site.register(Image)
 admin.site.register(Comment)
 admin.site.register(Profile)
 admin.site.register(Follow)
+admin.site.register(Like)
