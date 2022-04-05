316f751bb26eafb8b76b5e563fabff286f5045fc
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-05 07:39:29 -0700

    update tests

--- insta/tests.py
+++ insta/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Image, Profile
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.new_obj = User(username = "new_obj", email = "new_obj@gmail.com",password = "pass")
+        self.profile = Profile(bio='bio', user= self.new_obj)
+        self.new_obj.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_obj, User))
+        self.assertTrue(isinstance(self.profile, Profile))
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.new_obj = User(username = "new_obj", email = "new_obj@gmail.com",password = "pass")
+        self.profile = Profile(bio='bio', user= self.new_obj)
+
+        self.img = Image(image = 'imageurl', name ='img', caption = 'img-cap', profile = self.profile)
+        
+        self.new_obj.save()
+        self.profile.save()
+        self.img.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.img, Image))
